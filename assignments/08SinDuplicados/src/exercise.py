2af408a81c111fa06fe693a76ab6760756b728b9
Diego0103 <A01254073@itesm.mx>
2021-09-26 18:02:19 +0000

    08SinDuplicados

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,13 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
-    n=o
-    while n>0:
-        n=int(input(" "))
     lista=[]
-    print (lista[len(lista)-1])
+    n=int(input())
+    if n<=0:
+        print ("Error")
+    else: 
+        for i in range(n):
+            dato=input()
+            lista.append(dato)
+        print (lista)
+        
+        for i in len(lista):
+            if lista[i] in lista[i+1:]:
+                del lista[i]
+        print (lista)
 
 
-    pass
 
 if __name__=='__main__':
     main()
